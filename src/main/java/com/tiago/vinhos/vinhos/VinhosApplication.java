f4c6371d9f216850c64e38a6ffdadb11ffdc3941
Tiago Rosa da costa <tiagorosadacost@gmail.com>
2018-04-16 12:21:07 -0300

    Init project crud of wines

---
+++ src/main/java/com/tiago/vinhos/vinhos/VinhosApplication.java
@@ -0,0 +1,12 @@
+package com.tiago.vinhos.vinhos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinhosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VinhosApplication.class, args);
+    }
+}
---
+++ src/test/java/com/tiago/vinhos/vinhos/VinhosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tiago.vinhos.vinhos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VinhosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
